eeedafdb6940cc633c1de38dcf00fd4072c80516
johnpaulguzman <john_paul_guzman@dlsu.edu.ph>
2020-08-11 09:30:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Run as: bash <(curl --silent https://raw.githubusercontent.com/johnpaulguzman/termux-ytdl/master/setup.sh)
 set -e
 pkg install -y python ffmpeg termux-api
-termux-setup-storage
+pip install -U youtube-dl
 mkdir ~/bin
 curl --silent https://raw.githubusercontent.com/johnpaulguzman/termux-ytdl/master/termux-url-opener > ~/bin/termux-url-opener
 curl --silent https://raw.githubusercontent.com/johnpaulguzman/termux-ytdl/master/ytdl.py > ~/bin/ytdl.py
@@ -9,4 +9,5 @@ curl --silent https://raw.githubusercontent.com/johnpaulguzman/termux-ytdl/maste
 curl --silent https://raw.githubusercontent.com/johnpaulguzman/termux-ytdl/master/config.json > ~/bin/config.json
 
 chmod +x ~/bin/termux-url-opener
+termux-setup-storage
 echo "SUCCESS"
